299204ef2bbac9718a4831f5448f80b662a5102e
coderAdityasen <adityasen561@gmail.com>
2024-02-29 13:04:14 +0530

    blogfeed

--- src/Components/BlogsFeed.jsx
+++ src/Components/BlogsFeed.jsx
@@ -33,7 +33,7 @@ function BlogsFeed() {
           </h5>
         </a>
         <p className="font-normal text-gray-700 mb-3">
-         {`${item.description.slice(0,30)}...`}
+         {`${item.description.slice(0,50)}...`}
         </p>
         <button
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
